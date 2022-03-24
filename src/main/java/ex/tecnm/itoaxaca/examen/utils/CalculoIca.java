24779b50c29a729e3fbc2fba446f718a21a3f1d9
angel <angel@LAPTOP-HP4MHQ0L>
2022-03-23 22:48:10 -0600

    Examen de la u1

--- src/main/java/ex/tecnm/itoaxaca/examen/utils/CalculoIca.java
+++ src/main/java/ex/tecnm/itoaxaca/examen/utils/CalculoIca.java
@@ -44,19 +44,19 @@ public class CalculoIca {
                 if (ica < 0.34) {
                     nivel = "Delgadez Severa";
                 }
-                if (ica >= 0.35 || ica <= 0.42) {
+                else if (ica >= 0.35 && ica <= 0.42) {
                     nivel = "Delgadez Leve";
                 }
-                if (ica >= 0.43 || ica <= 0.52) {
+                else if (ica >= 0.43 && ica <= 0.52) {
                     nivel = "Peso Normal";
                 }
-                if (ica >= 0.53 || ica <= 0.57) {
+                else if (ica >= 0.53 && ica <= 0.57) {
                     nivel = "SobrePeso";
                 }
-                if (ica >= 0.58 || ica <= 0.62) {
+                else if (ica >= 0.58 && ica <= 0.62) {
                     nivel = "SobrePeso Elevado";
                 }
-                if (ica >= 0.63) {
+                else {
                     nivel = "Odesidad morbida";
                 }
                 break;
@@ -64,19 +64,19 @@ public class CalculoIca {
                 if (ica <= 0.34) {
                     nivel = "Delgadez Severa";
                 }
-                if (ica >= 0.35 || ica <= 0.41) {
+                else if (ica >= 0.35 && ica <= 0.41) {
                     nivel = "Delgadez Leve";
                 }
-                if (ica >= 0.42 || ica <= 0.48) {
+                else if (ica >= 0.42 && ica <= 0.48) {
                     nivel = "Peso Normal";
                 }
-                if (ica >= 0.49 || ica <= 0.53) {
+                else if (ica >= 0.49 && ica <= 0.53) {
                     nivel = "SobrePeso";
                 }
-                if (ica >= 0.54 || ica <= 0.57) {
+                else if (ica >= 0.54 && ica <= 0.57) {
                     nivel = "SobrePeso Elevado";
                 }
-                if (ica >= 0.58) {
+                else  {
                     nivel = "Odesidad morbida";
                 }
                 break;
